92883258fd3fb9e63f0da810a233f87d573464a5
Ecem <51851879+ecemisildar@users.noreply.github.com>
2022-12-18 22:34:14 +0100

    Add files via upload

---
+++ MOCOM_LAB2/include/GetDirectGeometry.m
@@ -0,0 +1,21 @@
+function [iTj_q] = GetDirectGeometry(q, biTri, linkType)
+%%% GetDirectGeometryFunction
+
+% Inputs: 
+% q : links current position ; 
+% iTj : vector of matrices containing the transformation matrices from link
+% i to link j
+% linkType: vector of size numberOfLinks identifying the joint type, 0 for revolute, 1 for
+% prismatic.
+
+% Outputs :
+% iTj_q vector of matrices containing the transformation matrices from link i to link j for the input q. 
+% The size of iTj is equal to (4,4,numberOfLinks)
+numberOfLinks = 7;
+
+
+for i = 1:1:numberOfLinks
+    iTj_q(:,:,i) = DirectGeometry(q(i),biTri(:,:,i), linkType(i));
+end
+
+end
